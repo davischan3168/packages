1c1784372f001982b4b5435a041f5746495c16ae
davischan3168 <racheal123@163.com>
2019-01-15 17:58:07 +0800

    upload yahoo-quoto

---
+++ yahoo/get_yahoo_quoto.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+"""
+get-yahoo-quotes.py:  Script to download Yahoo historical quotes using the new cookie authenticated site.
+
+ Usage: get-yahoo-quotes SYMBOL
+
+ History
+
+ 06-03-2017 : Created script
+
+"""
+
+__author__ = "Brad Luicas"
+__copyright__ = "Copyright 2017, Brad Lucas"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Brad Lucas"
+__email__ = "brad@beaconhill.com"
+__status__ = "Production"
+
+
+import re
+import sys
+import time
+import datetime
+import requests
+
+
+def split_crumb_store(v):
+    return v.split(':')[2].strip('"')
+
+
+def find_crumb_store(lines):
+    # Looking for
+    # ,"CrumbStore":{"crumb":"9q.A4D1c.b9
+    for l in lines:
+        if re.findall(r'CrumbStore', l):
+            return l
+    print("Did not find CrumbStore")
+
+
+def get_cookie_value(r):
+    return {'B': r.cookies['B']}
+
+
+def get_page_data(symbol):
+    url = "https://finance.yahoo.com/quote/%s/?p=%s" % (symbol, symbol)
+    r = requests.get(url)
+    cookie = get_cookie_value(r)
+
+    # Code to replace possible \u002F value
+    # ,"CrumbStore":{"crumb":"FWP\u002F5EFll3U"
+    # FWP\u002F5EFll3U
+    lines = r.content.decode('unicode-escape').strip(). replace('}', '\n')
+    return cookie, lines.split('\n')
+
+
+def get_cookie_crumb(symbol):
+    cookie, lines = get_page_data(symbol)
+    crumb = split_crumb_store(find_crumb_store(lines))
+    return cookie, crumb
+
+
+def get_data(symbol, start_date, end_date, cookie, crumb):
+    """
+    
+    """
+    filename = '%s.csv' % (symbol)
+    url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history&crumb=%s" % (symbol, start_date, end_date, crumb)
+    response = requests.get(url, cookies=cookie)
+    with open (filename, 'wb') as handle:
+        for block in response.iter_content(1024):
+            handle.write(block)
+
+
+def get_now_epoch():
+    """
+    获取当前时间
+    """
+    # @see https://www.linuxquestions.org/questions/programming-9/python-datetime-to-epoch-4175520007/#post5244109
+    return int(time.time())
+
+
+def download_quotes(symbol):
+    """
+    从雅虎财经上获取某只股票上市以来的交易价格和成交量
+    香港联交所的为：0001.HK
+    上海交易所的为：600685.SS
+    深圳交易所的为：000601.SZ
+    """
+    start_date = 0
+    end_date = get_now_epoch()
+    cookie, crumb = get_cookie_crumb(symbol)
+    get_data(symbol, start_date, end_date, cookie, crumb)
+
+
+if __name__ == '__main__':
+    # If we have at least one parameter go ahead and loop overa all the parameters assuming they are symbols
+    if len(sys.argv) == 1:
+        print("\nUsage: get-yahoo-quotes.py SYMBOL\n\n")
+    else:
+        for i in range(1, len(sys.argv)):
+            symbol = sys.argv[i]
+            print("--------------------------------------------------")
+            print("Downloading %s to %s.csv" % (symbol, symbol))
+            download_quotes(symbol)
+            print("--------------------------------------------------")
+
